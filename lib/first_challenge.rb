01d4763803c13b7fbbe28ae581814db84c729ed9
christine gomez <christine.gomez1226@gmail.com>
2018-09-08 22:19:14 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,7 +17,8 @@ def first_challenge
 contacts.each do |person, data|
   data.each do |attribute, value|
     if attribute == :favorite_icecream_flavors
-      value.each {|value| value.delete("strawberry")}
+      value.each {|value| value.delete("strawberry")} 
+    end
   #remember to return your newly altered contacts hash!
   contacts
 end
